f0a1da81e3814ca45b801cc7af5b8ddbc74bd2e4
Ryan Tran <73788695+RyanBoomer30@users.noreply.github.com>
2021-09-30 11:32:08 +0700

    Add files via upload

--- predict.py
+++ predict.py
@@ -5,22 +5,19 @@ import numpy as np
 import shutil
 
 
-# This folder contains a copy of data from source_image_folder defined just above,
+# This folder contains a copy of data from test_image_folder defined just above,
 # but in a structure permitting to train and test easily.
 # All good and bad folders will be created inside this folder. 
-# We can safely delete this folder if we want to run again the script,
-# by example when we change the criteria to classify Bad and Good data.
 processed_image_folder = "Data"
 
 # This folder is a child of processed_image_folder
 # This contains all the images that are being used to predict
-source_image_folder = "AnhThe"
+test_image_folder = "AnhThe"
 
-# This folder contains a copy of data from source_image_folder defined just above,
+# This folder contains a copy of data from test_image_folder defined just above,
 # but in a structure permitting to test easily.
 # All good and bad folders will be created inside this folder. 
 # We can safely delete this folder if we want to run again the script,
-# by example when we change the criteria to classify Bad and Good data.
 result_image_folder = "Result"
 goodResult_destination = os.path.join(processed_image_folder, result_image_folder, "Good")
 badResult_destination = os.path.join(processed_image_folder, result_image_folder, "Bad")
@@ -31,27 +28,28 @@ badResult_destination = os.path.join(processed_image_folder, result_image_folder
 os.makedirs(goodResult_destination)
 os.makedirs(badResult_destination)
 
-Categories = ["Good", "Bad"]
-
 Img_size = 50
 testing_data = []
 
 # Resize file
 def prepare(filepath):
     img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (Img_size, Img_size))
-    return new_array.reshape(-1, Img_size, Img_size, 1)
+    height, width = img_array.shape
+    crop_img = img_array[0:width, 0:width]
+    new_array = cv2.resize(crop_img, (Img_size, Img_size))
+    return np.array(new_array).reshape(-1, Img_size, Img_size, 1)
 
 # Load model
 model = tf.keras.models.load_model(processed_image_folder)
 
 # Generate predicted result into Good and Bad folders
-for i in os.listdir(source_image_folder):
-    test_image = os.path.join(source_image_folder, i)
-    predict = model.predict([prepare(test_image)])
-    if Categories[int(predict[0][0])] == "Good":
+for i in os.listdir(test_image_folder):
+    test_image = os.path.join(test_image_folder, i)
+    test_array = prepare(test_image) / 255
+    predict = model.predict(test_array)
+    if predict <= 0.5:
         dest = shutil.copy(test_image, goodResult_destination)
-        print("File copied: ", dest)
-    elif Categories[int(predict[0][0])] == "Bad":
+        print("File copied: ", predict, dest)
+    else:
         dest = shutil.copy(test_image, badResult_destination)
-        print("File copied: ", dest)
+        print("File copied: ", predict, dest)
